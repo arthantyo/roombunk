16e75e440e3e6aa8aa918a91db5f09352b201362
arthantyo <tyonirwansyah@gmail.com>
2026-08-24 18:52:05 +0300

    chore: add backend

--- backend/src/main/java/staycay/email/EmailService.java
+++ backend/src/main/java/staycay/email/EmailService.java
@@ -2,6 +2,7 @@ package staycay.email;
 
 import java.time.format.DateTimeFormatter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,12 @@ class EmailService {
             DateTimeFormatter.ofPattern("MMMM d, yyyy");
 
     private final JavaMailSender mailSender;
+    private final String senderAddress;
 
-    EmailService(JavaMailSender mailSender) {
+    EmailService(JavaMailSender mailSender,
+            @Value("${spring.mail.from:}") String senderAddress) {
         this.mailSender = mailSender;
+        this.senderAddress = senderAddress;
     }
 
     public void sendReservationConfirmationEmail(Reservation reservation) {
@@ -28,6 +32,9 @@ class EmailService {
         }
 
         SimpleMailMessage message = new SimpleMailMessage();
+        if (!senderAddress.isBlank()) {
+            message.setFrom(senderAddress);
+        }
         message.setTo(reservation.getUser().getEmail());
         message.setSubject("Staycay reservation confirmation #" + reservation.getId());
         message.setText(formatConfirmation(reservation));
